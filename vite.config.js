15a2deb97fc22131cdd6d94722214d2c267fd9d9
ankit singh <ankitanandsingh28@gmail.com>
2026-02-28 13:53:24 +0530

    fix: add base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/meme-generator/', // 👈 IMPORTANT (repo name)
   server: {
     port: 5173,
     open: true
   },
   css: {
-    postcss: './postcss.config.js', // Explicitly specify postcss config
+    postcss: './postcss.config.js',
   }
 })
